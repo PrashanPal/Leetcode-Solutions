8f3fde602435be8f2423b44904527e353d638cf4
PrashanPal <78657258+PrashanPal@users.noreply.github.com>
2022-04-20 07:20:22 +0530

    Time: 5 ms (98.45%), Space: 43.6 MB (88.48%) - LeetHub

--- 236-lowest-common-ancestor-of-a-binary-tree/236-lowest-common-ancestor-of-a-binary-tree.java
+++ 236-lowest-common-ancestor-of-a-binary-tree/236-lowest-common-ancestor-of-a-binary-tree.java
@@ -8,39 +8,32 @@
  * }
  */
 class Solution {
-    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-        if(root==null) return null;
-        ArrayList<TreeNode> ap=new ArrayList<>();
-        ArrayList<TreeNode> aq=new ArrayList<>();
-        h(root,ap,p);
-                h(root,aq,q);
-        int lp=ap.size();
-        int lq=aq.size();
-        for(int i=0;i<lq;i++){
-            for(int j=0;j<lp;j++){
-                if(ap.get(j)==aq.get(i)){
-                    return aq.get(i);
-                }//if
-            }//for
-        }//for
-        return null;
+    
+    public TreeNode backtrack(TreeNode root, TreeNode p, TreeNode q) {
+        
+        // the line below is the most hard to understand
+        // for the first level recursion, if p(or q) is exactly the whole tree's root, return the root(because root is the lowest node).
+        // for deeper(higher nodes) recursions, it indicates:
+        // (1) we find p(or q), which is fine because this node will not be returned(to output) instantly since it belongs to a deeper recursion but will be used for determinations of shallower(lower nodes) recursions
+        // (2) we reach the bottom of the tree, terminate the current track instantly and "null" will also be used for shallower recursions' determine statements.
+        if (root == null || root == p || root == q)
+            return root;
+        
+        TreeNode left = backtrack(root.left, p, q);
+        TreeNode right=backtrack(root.right, p, q);
+        
+        // determine statements
+        
+        if (left != null && right != null) // which means p,q exist below different subtrees;
+            return root;
+        
+        return left != null ? left : right; // which means p,q exist below the same subtree;
+        
     }
-    public void h(TreeNode r,ArrayList<TreeNode> a,TreeNode t){
-        if(r==null) return;
-        if(r==t){
-            a.add(r);
-            return;
-        }
-        h(r.left,a,t);
-        if(a.size()>0){
-            a.add(r);
-            return;
-        }
-         h(r.right,a,t);
-        if(a.size()>0){
-            a.add(r);
-            return;
-        }
-        return;
+    
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        
+        return backtrack(root, p, q);
+        
     }
 }
