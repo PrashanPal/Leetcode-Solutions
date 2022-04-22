9e649a937319d4c93af92be417dd739189e8cabd
PrashanPal <78657258+PrashanPal@users.noreply.github.com>
2022-04-22 12:32:53 +0530

    Time: 17 ms (82.09%), Space: 60.4 MB (30.05%) - LeetHub

--- 705-design-hashset/705-design-hashset.java
+++ 705-design-hashset/705-design-hashset.java
@@ -1,40 +1,34 @@
 class MyHashSet {
-    int num[];
+    boolean[] arr = new boolean[100];// start with 100 elements for fast initialization
+    /** Initialize your data structure here. */
     public MyHashSet() {
-        num = new int[31251];
+        
     }
-	
-	// set the bit if that element is present
+    
     public void add(int key) {
-        num[getIdx(key)]|=getMask(key);
+        if(key>=arr.length) // if array is too small to accomodate key, extend it.
+            extend(key);
+        arr[key]=true;
     }
-	
-	//unset the bit if a key is not present
+    
     public void remove(int key) {
-        num[getIdx(key)] &= (~getMask(key));
+        if(key>=arr.length) // if array is too small to accomodate key, extend it.
+            extend(key);
+        arr[key]=false;
     }
-	
-	//check if bit corresponding to the key is set or not
+    
+    /** Returns true if this set contains the specified element */
     public boolean contains(int key) {
-        return (num[getIdx(key)]&getMask(key))!=0;
+        if(key>=arr.length) // key cannot be in array if array's length < key
+            return false;
+        return arr[key]==true;
     }
-	
-	// idx of num[] to which this key belongs
-	// for key = 37, it will give 1
-    private int getIdx(int key)
-    {
-        return (key/32);
-    }
-	
-	// get mask representing the bit inside num[idx] that corresponds to given key.
-	// for key = 37, it will give 00000000000000000000000000100000
-    private int getMask(int key)
-    {
-        key%=32;
-        return (1<<key);
+    
+    public void extend(int key){
+        arr= Arrays.copyOf(arr, key+2);  // extend array to one more item than necessary, we need "key" items. 
+									     // we give "key+1" items to reduce collisions.
     }
 }
-
 /**
  * Your MyHashSet object will be instantiated and called as such:
  * MyHashSet obj = new MyHashSet();
