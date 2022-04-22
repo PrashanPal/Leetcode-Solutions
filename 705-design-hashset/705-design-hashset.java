c9bb1805667a9424e80d122e1b0ef01687c6b991
PrashanPal <78657258+PrashanPal@users.noreply.github.com>
2022-04-22 12:18:46 +0530

    Time: 22 ms (67.24%), Space: 56.6 MB (56.54%) - LeetHub

--- 705-design-hashset/705-design-hashset.java
+++ 705-design-hashset/705-design-hashset.java
@@ -1,24 +1,37 @@
-class MyHashSet extends HashSet<Integer>{
-   boolean b[]=new boolean[10000001];;
-       public MyHashSet() {
-       //MyHashSet a=new     MyHashSet();
-         // b =new boolean[10000001];
-          // int p=5;
+class MyHashSet {
+    int num[];
+    public MyHashSet() {
+        num = new int[31251];
     }
-    
+	
+	// set the bit if that element is present
     public void add(int key) {
-        b[key]=true;
+        num[getIdx(key)]|=getMask(key);
     }
-    
+	
+	//unset the bit if a key is not present
     public void remove(int key) {
-        if(b[key]){
-            b[key]=false;
-        }
+        num[getIdx(key)] &= (~getMask(key));
     }
-    
+	
+	//check if bit corresponding to the key is set or not
     public boolean contains(int key) {
-       // System.out.print(p+" ");
-        return b[key];
+        return (num[getIdx(key)]&getMask(key))!=0;
+    }
+	
+	// idx of num[] to which this key belongs
+	// for key = 37, it will give 1
+    private int getIdx(int key)
+    {
+        return (key/32);
+    }
+	
+	// get mask representing the bit inside num[idx] that corresponds to given key.
+	// for key = 37, it will give 00000000000000000000000000100000
+    private int getMask(int key)
+    {
+        key%=32;
+        return (1<<key);
     }
 }
 
