1bbdb09eea55454eee345daa04670b9f5366446a
PrashanPal <78657258+PrashanPal@users.noreply.github.com>
2022-05-15 11:07:56 +0530

    Time: 1 ms (100.00%), Space: 44.7 MB (88.87%) - LeetHub

--- 1302-deepest-leaves-sum/1302-deepest-leaves-sum.java
+++ 1302-deepest-leaves-sum/1302-deepest-leaves-sum.java
@@ -15,23 +15,40 @@
  */
 class Solution {
     public int deepestLeavesSum(TreeNode root) {
-        //List<List<Integer>> a= new ArrayList<>();
-        Queue<TreeNode> q=new ArrayDeque<>();
-        q.add(root);
-        int sum=0;
-        while(q.size()>0){
-            sum=0;
-        int s=q.size();
-        while(s>0){
-            TreeNode t=q.remove();
-            if(t.left!=null)
-            q.add(t.left);
-            if(t.right!=null)
-             q.add(t.right);
-            s--;
-            sum=sum+t.val;
-        }//while
-        }//while
-        return sum;
+        int[] maxDepth = new int[1];
+        int[] sumOfDeepestLeaves = new int[1];
+        findDepth(root, maxDepth, sumOfDeepestLeaves, 1);
+        return sumOfDeepestLeaves[0];
     }
+    
+    public void findDepth(TreeNode root, int[] maxDepth, int[] sumOfDeepestLeaves, 
+                          int currDepth) {
+        if(root == null) {
+           return; 
+        }
+        
+        if(root.left == null && root.right == null) {
+            /** If depth of the current leaf is equal to existing maximum depth, 
+                add the value of this leaf to the existing sum of deepest leaves. */
+            if(maxDepth[0] == currDepth) {
+                sumOfDeepestLeaves[0] += root.val;
+                return;
+            /** If depth of the current leaf is less than the existing maximum depth,
+                dont change the existing sum of deepest leaves and return. */
+            } else if(currDepth < maxDepth[0]) {
+                return;
+            /** If depth of the current leaf is greater than the existing maximum depth,
+                set max depth to current depth and also initialize the sum of deepest leaves
+                as the current node val */
+            } else {
+                sumOfDeepestLeaves[0] = root.val;
+                maxDepth[0] = currDepth;
+                return;
+            }
+        }
+        
+        findDepth(root.left, maxDepth, sumOfDeepestLeaves, currDepth+1);
+        findDepth(root.right, maxDepth, sumOfDeepestLeaves, currDepth+1);
+        return;
+    } 
 }
