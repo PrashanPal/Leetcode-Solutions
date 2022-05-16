fc53d2bef9c0e5b4bd694fa11a86ad6284cd605c
PrashanPal <78657258+PrashanPal@users.noreply.github.com>
2022-05-16 09:40:18 +0530

    Time: 5 ms (30.90%), Space: 46.5 MB (45.95%) - LeetHub

--- 572-subtree-of-another-tree/572-subtree-of-another-tree.java
+++ 572-subtree-of-another-tree/572-subtree-of-another-tree.java
@@ -14,15 +14,16 @@
  * }
  */
 class Solution {
+    //Time complexity=m*n , m=no. of node in main tree and n=no. node in subtree
+    
     public boolean isSubtree(TreeNode root, TreeNode subRoot) {
         if(root==null) return false;
-        // if(root.val==subRoot.val){
-        //     return c(root,subRoot);
-        // }
+        if(root.val==subRoot.val){
          if(c(root,subRoot)) return true;
+        }// ie if our result is true then no need to treverse this branch ,now we will traverse in different branch
         return isSubtree(root.left,subRoot)||isSubtree(root.right,subRoot);
     }
-    public boolean c(TreeNode r,TreeNode sr){
+    public boolean c(TreeNode r,TreeNode sr){//to check that the trees are identical or not
         if(r==null&&sr==null) return true;
         else if(r!=null&&sr==null) return false;
        else if(r==null&&sr!=null) return false;
