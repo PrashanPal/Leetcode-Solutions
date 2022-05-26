c36942541d3253f15105f4db9d1cfaedaa6b8517
PrashanPal <78657258+PrashanPal@users.noreply.github.com>
2022-05-26 14:53:47 +0530

    Time: 1 ms (82.89%), Space: 48.2 MB (37.52%) - LeetHub

--- 733-flood-fill/733-flood-fill.java
+++ 733-flood-fill/733-flood-fill.java
@@ -1,37 +1,16 @@
 class Solution {
-    class Pair{
-        int i;
-        int j;
-        Pair(int i,int j){
-            this.i=i;
-            this.j=j;
-        }
-    }
     public int[][] floodFill(int[][] image, int sr, int sc, int newColor) {
-        int v=image[sr][sc];
-        int r=image.length;
-        int c=image[0].length;
-        if(v==newColor) return image;
-        Queue<Pair> q=new ArrayDeque<>();
-        q.add(new Pair(sr,sc));
-        while(q.size()>0){
-            Pair t=q.poll();
-            if(image[t.i][t.j]==v){
-                image[t.i][t.j]=newColor;
-                if(t.i+1<r&&image[t.i+1][t.j]==v){
-                    q.add(new Pair(t.i+1,t.j));
-                }
-                if(t.j+1<c&&image[t.i][t.j+1]==v){
-                    q.add(new Pair(t.i,t.j+1));
-                }
-                if(t.i-1>=0&&image[t.i-1][t.j]==v){
-                    q.add(new Pair(t.i-1,t.j));
-                }
-                if(t.j-1>=0&&image[t.i][t.j-1]==v){
-                    q.add(new Pair(t.i,t.j-1));
-                }
-            }
-        }
+        if (image[sr][sc] == newColor) return image;
+        fill(image, sr, sc, image[sr][sc], newColor);
         return image;
+    }
+    
+    private void fill(int[][] image, int sr, int sc, int color, int newColor) {
+        if (sr < 0 || sr >= image.length || sc < 0 || sc >= image[0].length || image[sr][sc] != color) return;
+        image[sr][sc] = newColor;
+        fill(image, sr + 1, sc, color, newColor);
+        fill(image, sr - 1, sc, color, newColor);
+        fill(image, sr, sc + 1, color, newColor);
+        fill(image, sr, sc - 1, color, newColor);
     }
 }
