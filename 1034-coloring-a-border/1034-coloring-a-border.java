cce248c8f84089ba5fb1106c0b47d3875ea31f96
PrashanPal <78657258+PrashanPal@users.noreply.github.com>
2022-05-26 20:09:55 +0530

    Time: 0 ms (100.00%), Space: 43.2 MB (84.82%) - LeetHub

--- 1034-coloring-a-border/1034-coloring-a-border.java
+++ 1034-coloring-a-border/1034-coloring-a-border.java
@@ -1,30 +1,38 @@
 class Solution {
+    // METHODE 1:simple brute force approach by me
+    //     problem: it uses an extra boolean array b to mark visited ,because if mark in the same grid by color,it will mark all the to,color which are connect
     public int[][] colorBorder(int[][] grid, int r0, int c0, int color) {
-        if(grid == null || grid.length == 0)
-            return null;
-        int oldColor = grid[r0][c0];
-        dfs(grid, r0, c0, oldColor);
-        for(int i = 0; i < grid.length; i++) {
-            for(int j = 0; j < grid[0].length; j++) {
-                if(grid[i][j] < 0)
-                    grid[i][j] = color;
-            }
-        }
+        int v=grid[r0][c0];//color to be replaced
+            boolean b[][]=new boolean[grid.length][grid[0].length];
+       h(grid,r0,c0,color,v,b);
         return grid;
     }
-    public void dfs(int[][] grid, int i, int j, int oldColor) {
-        if(i < 0 || j < 0 || i >= grid.length || j >= grid[0].length || grid[i][j] != oldColor) 
-            return;
-        grid[i][j] = -oldColor;
-        dfs(grid, i+1, j, oldColor);
-        dfs(grid, i-1, j, oldColor);
-        dfs(grid, i, j+1, oldColor);
-        dfs(grid, i, j-1, oldColor);
-        if(i > 0 && j > 0 && i < grid.length-1 && j < grid[0].length-1
-           && oldColor == Math.abs(grid[i+1][j])
-           && oldColor == Math.abs(grid[i-1][j])
-           && oldColor == Math.abs(grid[i][j+1])
-           && oldColor == Math.abs(grid[i][j-1]))
-            grid[i][j] = oldColor;
-    }
+     public  void h(int [][] a, int r, int c , int col,int v,boolean b[][]){
+    if(v==col) return;
+    if(r<0||c<0||r==a.length||c==a[0].length){
+        return;
+      }
+      if(a[r][c]==v&&b[r][c]==false){
+      b[r][c]=true;
+      if(r==0||c==0||r+1==a.length||c+1==a[0].length){
+        a[r][c]=col;
+      }
+      else if(r-1>=0&&a[r-1][c]!=v&&b[r-1][c]==false){
+        a[r][c]=col;
+      }
+       else if(c-1>=0&&a[r][c-1]!=v&&b[r][c-1]==false){
+        a[r][c]=col;
+      }
+       else if(r+1<a.length&&a[r+1][c]!=v&&b[r+1][c]==false){
+        a[r][c]=col;
+      }
+       else if(c+1<a[0].length&&a[r][c+1]!=v&&b[r][c+1]==false){
+        a[r][c]=col;
+      }
+      h(a,r-1,c,col,v,b);
+      h(a,r,c-1,col,v,b);
+      h(a,r+1,c,col,v,b);
+      h(a,r,c+1,col,v,b);
+      }//final if
+  }//fn
 }
