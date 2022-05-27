c7628fe615607b4b2a150324606cbe3929989bb5
PrashanPal <78657258+PrashanPal@users.noreply.github.com>
2022-05-27 21:38:44 +0530

    Time: 19 ms (54.54%), Space: 73.4 MB (28.95%) - LeetHub

--- 542-01-matrix/542-01-matrix.java
+++ 542-01-matrix/542-01-matrix.java
@@ -1,34 +1,49 @@
-public class Solution {
+class Solution {
+    // method2
+    //Very important TOOL and CONCEPT
+    //Approach used BFS i,e, radius wise Propagation
+     private static class Pair {
+    int x;
+    int y;
+
+    Pair(int x, int y) {
+      this.x = x;
+      this.y = y;
+    }
+  }
+      private static int[][] dirs = new int[][] { { 1, 0 }, { -1, 0 }, { 0, 1 }, { 0, -1 } };
     public int[][] updateMatrix(int[][] matrix) {
-        int m = matrix.length;
-        int n = matrix[0].length;
-        
-        Queue<int[]> queue = new LinkedList<>();
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if (matrix[i][j] == 0) {
-                    queue.offer(new int[] {i, j});
-                }
-                else {
-                    matrix[i][j] = Integer.MAX_VALUE;
-                }
-            }
+        LinkedList<Pair> queue = new LinkedList<>();
+
+    for (int i = 0; i < matrix.length; i++) {
+      for (int j = 0; j < matrix[0].length; j++) {
+        if (matrix[i][j] == 1) {
+          matrix[i][j] = -1;// initaially mark all the 1s as unvisited
+        } else {
+          queue.addLast(new Pair(i, j));// add 0 valued cell in queue
         }
-        
-        int[][] dirs = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
-        
-        while (!queue.isEmpty()) {
-            int[] cell = queue.poll();
-            for (int[] d : dirs) {
-                int r = cell[0] + d[0];
-                int c = cell[1] + d[1];
-                if (r < 0 || r >= m || c < 0 || c >= n || 
-                    matrix[r][c] <= matrix[cell[0]][cell[1]] + 1) continue;
-                queue.add(new int[] {r, c});
-                matrix[r][c] = matrix[cell[0]][cell[1]] + 1;
-            }
-        }
-        
-        return matrix;
+      }
+    }
+    int length = 0;//radius of the BFS
+    while (!queue.isEmpty()) {
+      int size = queue.size();
+      length++;
+      while(size>0) {
+        Pair rem = queue.removeFirst();
+          size--;
+        for (int[] dir : dirs) {
+          int idash = rem.x + dir[0];
+          int jdash = rem.y + dir[1];
+
+          if (idash >= 0 && jdash >= 0 && idash < matrix.length && jdash < matrix[0].length) {
+            if (matrix[idash][jdash] == -1) {
+              matrix[idash][jdash] = length;
+              queue.addLast(new Pair(idash, jdash));
+            }//if
+          }//fi
+        }//for
+      }//for
+    }//while
+    return matrix;
     }
 }
