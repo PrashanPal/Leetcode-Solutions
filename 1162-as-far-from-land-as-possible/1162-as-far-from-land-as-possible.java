16a5b25c7df8abd13a1237a8d9f917ca1de0ea83
PrashanPal <78657258+PrashanPal@users.noreply.github.com>
2022-05-28 10:57:13 +0530

    Time: 16 ms (74.13%), Space: 43.4 MB (84.51%) - LeetHub

---
+++ 1162-as-far-from-land-as-possible/1162-as-far-from-land-as-possible.java
@@ -0,0 +1,57 @@
+class Solution {
+    //542 0-1 matrix (compare these 2 question for concept understanding)
+    //big TOOL-->multisource BFS
+    // notes2--page39
+    // question ask to find--->maximum distance of land from water ,i.e. maximum distance of 0(water cell) from         1(land cell)
+    //Approach---> find maximum possible radius of multisource BFS of 1
+    class p{
+        int x;
+        int y;
+        p(int x,int y){
+            this.x=x;
+            this.y=y;
+        }//p
+    }//class
+     public int maxDistance(int[][] grid) {
+            Queue<p> q=new ArrayDeque<>();
+        for(int i=0;i<grid.length;i++){
+            for(int j=0;j<grid[0].length;j++){
+                if(grid[i][j]==1)//
+                    q.add(new p(i,j));
+            }//for
+        }//for
+        int r=h(grid,q);
+         if(r>0) return r;
+         return -1;
+        
+    }//fn
+        public int h(int[][] g, Queue<p> q){
+            int l=0;//to count no. of hours
+                while(q.size()>0){
+                    int s=q.size();
+                    while(s>0){
+                        p t=q.poll();
+                        s--;
+            if(t.x-1>=0&&g[t.x-1][t.y]==0){
+                g[t.x-1][t.y]=1;//to mark it visited or rotten
+                    q.add(new p(t.x-1,t.y));
+                }//if
+                        if(t.y-1>=0&&g[t.x][t.y-1]==0){
+                g[t.x][t.y-1]=1;//to mark it visited or rotten
+                    q.add(new p(t.x,t.y-1));
+                }//if
+                        if(t.x+1<g.length&&g[t.x+1][t.y]==0){
+                g[t.x+1][t.y]=1;//to mark it visited or rotten
+                    q.add(new p(t.x+1,t.y));
+                }//if
+                        if(t.y+1<g[0].length&&g[t.x][t.y+1]==0){
+                g[t.x][t.y+1]=1;//to mark it visited or rotten
+                    q.add(new p(t.x,t.y+1));
+                }//if
+                   
+            }//while
+                    if(q.size()>0) l++;
+                }//while
+             return l;
+        }//fn
+    }//class
