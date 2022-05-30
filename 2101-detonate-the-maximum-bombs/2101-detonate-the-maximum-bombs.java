378ce720df231a5458a9922f5207ea59f5ae3227
PrashanPal <78657258+PrashanPal@users.noreply.github.com>
2022-05-30 19:04:42 +0530

    Time: 155 ms (49.73%), Space: 44.8 MB (66.85%) - LeetHub

--- 2101-detonate-the-maximum-bombs/2101-detonate-the-maximum-bombs.java
+++ 2101-detonate-the-maximum-bombs/2101-detonate-the-maximum-bombs.java
@@ -1,11 +1,14 @@
 class Solution {
+    //apprroach: multisourcr BFS where we put all the bomb in queue for BFS that are in the choosen       bomb   
      int count = 0; // global variable, otherwise use an array
+    
     public int maximumDetonation(int[][] bombs) {
         int n = bombs.length;
         int ans = 0;
         for (int i = 0; i < n; i++) {
             count = 0; // Start a new search, reset count
             dfs(i, new boolean[n], bombs);
+            //we do the whole process for each bomb an return the maximum among the all values
             ans = Math.max(ans, count);
         }
         return ans;
@@ -18,12 +21,14 @@ class Solution {
         for (int i = 0; i < n; i++) {
             if (!v[i] && inRange(bombs[idx], bombs[i])) {
                 v[i] = true;
-                dfs(i, v, bombs);
+                dfs(i, v, bombs);//dfs based approach
             }
         }
     }
 
     private boolean inRange(int[] a, int[] b) {
+        //it return that the bomb is in the of previous bom or not
+        //formula used=(x-x1)^2 + (y-y1)^2=r*r;
         long dx = a[0] - b[0], dy = a[1] - b[1], r = a[2];
         return dx * dx + dy * dy <= r * r;
     }
