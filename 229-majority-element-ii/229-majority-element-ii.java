8b7a5107ea1e671af8692d7b641763af1b02bef6
PrashanPal <78657258+PrashanPal@users.noreply.github.com>
2022-07-03 11:46:26 +0530

    Time: 4 ms (60.05%), Space: 49.3 MB (73.49%) - LeetHub

--- 229-majority-element-ii/229-majority-element-ii.java
+++ 229-majority-element-ii/229-majority-element-ii.java
@@ -14,8 +14,7 @@ class Solution {
         for(int i=1;i<nums.length;i++){
             if(nums[i]==val1) count1++;
             else if(nums[i]==val2) count2++;
-            else{
-                if(count1==0){
+                else if(count1==0){
                     val1=nums[i];
                     count1=1;
                 }
@@ -27,7 +26,6 @@ class Solution {
                     count1--;
                     count2--;
                 }//else
-            }//else
         }//for
         int n=nums.length;
             //since val1 and val2 are potential answer not the actual ,so we have to check both
