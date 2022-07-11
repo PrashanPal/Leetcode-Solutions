c7a025c1c2b627cc1f2b594c0495aafe0e3c050c
PrashanPal <78657258+PrashanPal@users.noreply.github.com>
2022-07-11 17:53:26 +0530

    Time: 14 ms (98.02%), Space: 57.7 MB (87.78%) - LeetHub

--- 523-continuous-subarray-sum/523-continuous-subarray-sum.java
+++ 523-continuous-subarray-sum/523-continuous-subarray-sum.java
@@ -1,43 +1,34 @@
 class Solution {
-    //this question is similar to 974 i.e sub arrays sum which is divisble by k
-    //there are two methods to do ,1--> by using frequency of remainder of prefix sum ,2--> by using sum of frequencies of elements
-    // since i do 974 by prefixsum i.e method one,so i am doing it with method 2 i.e using remainder of elements as prefix sum
-    public boolean checkSubarraySum(int[] nums, int k) {
-        //insted of using hashmap i am using here an array of size k,for storing freqencies of elements where idex are the remainders
-    HashMap<Integer,Integer> h=new HashMap<>();
-        int s=0;
-        int c=0;//count of subarrays whoes sum is divisible by k
-        h.put(0,1);//it's important ,to do
-        int d=0;//for storing remainder
-        int f=0;//it is used as a flag here ,which indicates that array contains an element which is directly divisivle k
-        for(int i=0;i<nums.length;i++){
-             d=nums[i]%k;
-            if(d<0) d=d+k;
-            s=(s+d)%k;
-   
-            if(!h.containsKey(s)){
-             c=c+0;//line 1-->this is the most important line
-                h.put(s,1);
-            }
-            else if(h.containsKey(s)){
-             c=c+h.get(s);//line 1-->this is the most important line
-                h.put(s,h.get(s)+1);//line 2-->this must came after line 1
-            }
-            if(d==0) f++;
-        }//for
-            if(f>0){
-                if(c>f) //nice trik by me,ie. if count greater then total no. of elements which are exactly divisible by k, because if an elements is divisble by k ,then in that case lenght of subarray can be 1, and we have count only those subarrays whoes length is at least 2
-                    return true;
-                else
-                    return false;
-            }//if
-            else if(f==0){
-                if(c>0) return true;
-                else return false;
-            }//else if
-            return false;
+    //this is method 2 and more optimized , method 1 is already sumitted
+   /** Key point: if we can find any two subarray of prefix sum have same mod value, then their difference MUST be
+ * divisible by k. So we can use a map to store mod value of each prefix sum in map, with its index. Then check
+ * if map contains the same mod value with size > 2 when we have new mod value in every iteration */
+public boolean checkSubarraySum(int[] nums, int k) {
+    if (nums.length < 2) {
+        return false;
     }
+
+    Map<Integer, Integer> map = new HashMap<>();
+    // corner case: if the very first subarray with first two numbers in array could form the result, we need to 
+    // put mod value 0 and index -1 to make it as a true case
+    map.put(0, -1);
+    int curSum = 0;
+    for (int i = 0; i < nums.length; i++) {
+        curSum += nums[i];
+
+        // corner case: k CANNOT be 0 when we use a number mod k
+        if (k != 0) {
+            curSum = curSum % k;
+        }
+        if (map.containsKey(curSum)) {
+            if (i - map.get(curSum) > 1) {
+                return true;
+            }
+        }
+        else {
+            map.put(curSum, i);
+        }
+    }
+    return false;
 }
-//exm-->[23,2,4,6,7,13,13] k=13
-//in this case count =3 as subarrays are [13],[13],[13,13]
-//and f=2 ,sice f>0 and c>f i.e 3>2 ,so our result is true;
+}
