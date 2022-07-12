91d8dd90b6641c67c88ab0e15836e680d86c15dc
PrashanPal <78657258+PrashanPal@users.noreply.github.com>
2022-07-12 18:27:58 +0530

    Time: 1 ms (69.61%), Space: 48.1 MB (73.64%) - LeetHub

--- 867-transpose-matrix/867-transpose-matrix.java
+++ 867-transpose-matrix/867-transpose-matrix.java
@@ -1,11 +1,41 @@
+// class Solution {
+//     public int[][] transpose(int[][] matrix) {
+//         int b[][]=new int[matrix[0].length][matrix.length];
+//         for(int i=0;i<matrix.length;i++){
+//             for(int j=0;j<matrix[0].length;j++){
+//                 b[j][i]=matrix[i][j];
+//             }//for
+//         }//for
+//         return b;
+//     }
+// }
+
 class Solution {
-    public int[][] transpose(int[][] matrix) {
-        int b[][]=new int[matrix[0].length][matrix.length];
-        for(int i=0;i<matrix.length;i++){
-            for(int j=0;j<matrix[0].length;j++){
-                b[j][i]=matrix[i][j];
-            }//for
-        }//for
-        return b;
-    }
+    //this method is little more optimized than me , as it have additional case to handle inplace it n==m i.e
+    // a square matrix can be transposed in o(1) space i.e. inplace
+public int[][] transpose(int[][] A) {
+		if (A.length == A[0].length) {
+				inPlace(A);
+				return A;
+		}        
+		int[][] B = new int[A[0].length][A.length];
+		for (int i=0; i<A.length ; i++) {
+				for (int j=0; j<A[0].length ; j++) {
+						B[j][i] = A[i][j];
+				}
+		}        
+		return B;
 }
+
+private void inPlace(int[][] A) {
+		int col = 0;
+		for (int i = 0; i<A.length ; i++) {
+				for (int j=col ; j<A[0].length ; j++) {
+						int temp = A[i][j];
+						A[i][j] = A[j][i];
+						A[j][i] = temp;
+			}//for
+				col++;
+	}//for
+}//inPlace
+}//class
