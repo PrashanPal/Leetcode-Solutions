5d5245709f50c5ad154daf0baa1b10e7b59d14e4
PrashanPal <78657258+PrashanPal@users.noreply.github.com>
2022-07-17 18:03:00 +0530

    Time: 11 ms (58.28%), Space: 42.8 MB (41.86%) - LeetHub

--- 688-knight-probability-in-chessboard/688-knight-probability-in-chessboard.java
+++ 688-knight-probability-in-chessboard/688-knight-probability-in-chessboard.java
@@ -1,38 +1,39 @@
 class Solution {
-    //after reading solution
     //concept:great dp solution p2
     //n2--105
     //previous trival dfs traversing approach will not work here
-    int [][] direction =new int[][]{{2,1},{-2,1},{2,-1},{-2,-1},{1,2},{1,-2},{-1,2},{-1,-2}};
-   
-    public double knightProbability(int N, int K, int r, int c) {
-        double [][] current = new double[N][N];//it is of double type because it have to store probability
-            double [][] next = new double[N][N];//it becames next
-        //since initially i.e at 0 moves knight is at current[r][c], at initally probability at current[r][c]=1
-        current[r][c]=1;
-      
-        for(int k=1; k<=K;++k){
-        //    double [][] next = new double[N][N];//it becames next
-            for(int i=0; i<N;++i){
-                for(int j=0; j<N;++j){
-                    for(int [] dir: direction){
-                        int nextR = i-dir[0];
-                        int nextC = j-dir[1];
-                        if(nextR>=0 && nextC>=0 && nextR<N && nextC<N){
-                            next[i][j]+=(current[nextR][nextC]/8.0);
+    static int a[][]={{1,-2},{2,-1},{2,1},{1,2},{-1,2},{-2,1},{-2,-1},{-1,-2}};
+    public double knightProbability(int n, int k, int row, int column) {
+       
+       //we will take to grid i.e current and next,from modifying current we will get next
+        double cur[][]=new double[n][n];//this of double type because it have to store probability
+         double next[][]=new double[n][n];//this of double type because it have to store probability
+        //since initially knight is at cur[row][column] so its probability is 1 at 0 moves
+        cur[row][column]=1;
+        for(int m=1;m<=k;m++){//i.e for m times i.e for each cycle of moves till m times we have calculate next using current
+            for(int i=0;i<n;i++){
+                for(int j=0;j<n;j++){
+                    int nexti=0;
+                    int nextj=0;
+                    for(int p=0;p<8;p++){
+                        nexti=i+a[p][0];
+                        nextj=j+a[p][1];
+                        if(nexti>=0&&nexti<n&&nextj>=0&&nextj<n){
+                            next[nexti][nextj]+=cur[i][j]/8.0;
                         }//if
-                    }//for dir
+                    }//for p
                 }//for j
-            }//for i
-            current=next;//current becames next
-            next=new double[N][N];
-        }
-          double sum = 0;
-        for(int i=0; i<N;++i){// to calculate total probability i.e sum of probability at each cell of current after k moves will be our required ans
-            for(int j=0; j<N;++j){
-                sum+=current[i][j];
-            }
-        }
+            }//for i 
+                //after each move next becames current and new next is assigd to previous next
+                cur=next;
+                next=new double[n][n];
+            }//for ,final for .ie. m
+            double sum=0;//for calculate probability of each cell in cur
+            for(int i=0;i<n;i++)
+                for(int j=0;j<n;j++){
+                   // System.out.print(cur[i][j]+" ");
+                    sum+=cur[i][j];
+                }
         return sum;
-    }
+    }//
 }//class
