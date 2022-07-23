d59cbc726b632b33cef417610812113b819959fe
PrashanPal <78657258+PrashanPal@users.noreply.github.com>
2022-07-23 17:11:28 +0530

    Time: 664 ms (14.43%), Space: 47 MB (85.91%) - LeetHub

---
+++ 15-3sum/15-3sum.java
@@ -0,0 +1,20 @@
+class Solution {
+    //nice trick
+         public  List<List<Integer>> threeSum(int[] nums) {
+        Set<List<Integer>> res  = new HashSet<>();//it will avoid repetition of same triplet pairs
+        if(nums.length==0) return new ArrayList<>(res);
+        Arrays.sort(nums);
+        for(int i=0; i<nums.length-2;i++){
+            int j =i+1;
+           int  k = nums.length-1;
+            while(j<k){
+                int sum = nums[i]+nums[j]+nums[k];
+                if(sum==0)res.add(Arrays.asList(nums[i],nums[j++],nums[k--]));
+                else if ( sum >0) k--;
+                else if (sum<0) j++;
+            }
+
+        }
+        return new ArrayList<>(res);
+    }
+}
