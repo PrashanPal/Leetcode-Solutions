0f14de70e12a6a8031f377067ae47a48e32be0d0
PrashanPal <78657258+PrashanPal@users.noreply.github.com>
2022-07-23 17:18:41 +0530

    Time: 37 ms (48.73%), Space: 57.3 MB (82.91%) - LeetHub

--- 15-3sum/15-3sum.java
+++ 15-3sum/15-3sum.java
@@ -1,20 +1,32 @@
 class Solution {
     //nice trick
+    //must read it--this is method-2
          public  List<List<Integer>> threeSum(int[] nums) {
-        Set<List<Integer>> res  = new HashSet<>();//it will avoid repetition of same triplet pairs
-        if(nums.length==0) return new ArrayList<>(res);
-        Arrays.sort(nums);
-        for(int i=0; i<nums.length-2;i++){
-            int j =i+1;
-           int  k = nums.length-1;
-            while(j<k){
-                int sum = nums[i]+nums[j]+nums[k];
-                if(sum==0)res.add(Arrays.asList(nums[i],nums[j++],nums[k--]));
-                else if ( sum >0) k--;
-                else if (sum<0) j++;
-            }
-
+       // see method-1--->Set<List<Integer>> res  = new HashSet<>();//it will avoid repetition of same triplet pairs
+             
+             //this is the most easy solution avialable
+         List<List<Integer>> res = new ArrayList<>();
+    Arrays.sort(nums);
+    for (int i = 0; i + 2 < nums.length; i++) {
+        if (i > 0 && nums[i] == nums[i - 1]) {              // skip same result
+            continue;
         }
-        return new ArrayList<>(res);
+        int j = i + 1, k = nums.length - 1;  
+        int target = -nums[i];
+        while (j < k) {
+            if (nums[j] + nums[k] == target) {
+                res.add(Arrays.asList(nums[i], nums[j], nums[k]));
+                j++;
+                k--;
+                while (j < k && nums[j] == nums[j - 1]) j++;  // skip same result ,these two steps just avoid the need for using hash set for avoiding repetition
+                while (j < k && nums[k] == nums[k + 1]) k--;  // skip same result
+            } else if (nums[j] + nums[k] > target) {
+                k--;
+            } else {
+                j++;
+            }
+        }
+    }
+    return res;
     }
 }
