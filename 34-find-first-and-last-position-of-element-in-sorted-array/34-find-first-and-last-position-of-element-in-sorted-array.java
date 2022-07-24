62997c8f797e0fbdecd3d8b3b5df2b3b5804c630
PrashanPal <78657258+PrashanPal@users.noreply.github.com>
2022-07-24 11:03:58 +0530

    Time: 0 ms (100.00%), Space: 47.3 MB (39.99%) - LeetHub

--- 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.java
+++ 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.java
@@ -32,24 +32,24 @@ class Solution {
             a[1]=-1;
             return a;
         }//if
-        else if(mid==nums.length-1&&nums[mid]==target){//end point
-            a[1]=mid;
-             int t=mid-1;
-             while(t>=0&&nums[t]==target){
-                 t=t-1;
-             }//while
-             a[0]=t+1;
-        }//else if
-         else if(mid==0&&nums[mid]==target){//start point
-            a[0]=mid;
-            int t=mid+1;
-             while(t<nums.length&&nums[t]==target){
-                 t=t+1;
-             }//while
-             a[1]=t-1;
+//         else if(mid==nums.length-1&&nums[mid]==target){//end point
+//             a[1]=mid;
+//              int t=mid-1;
+//              while(t>=0&&nums[t]==target){
+//                  t=t-1;
+//              }//while
+//              a[0]=t+1;
+//         }//else if
+//          else if(mid==0&&nums[mid]==target){//start point
+//             a[0]=mid;
+//             int t=mid+1;
+//              while(t<nums.length&&nums[t]==target){
+//                  t=t+1;
+//              }//while
+//              a[1]=t-1;
              
-        }//else if
-        else if(mid>0&&mid<nums.length-1&&nums[mid]==target){
+//         }//else if
+        else if(mid>=0&&mid<=nums.length-1&&nums[mid]==target){
             int l=mid-1;//for checking left
             int r=mid+1;//for checking right
             while(l>=0&&nums[l]==target){
