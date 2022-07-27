655abe82af1495cae3b012567309b5e2c01cd61d
PrashanPal <78657258+PrashanPal@users.noreply.github.com>
2022-07-27 12:08:24 +0530

    Time: 1 ms (86.78%), Space: 40.7 MB (67.16%) - LeetHub

---
+++ 670-maximum-swap/670-maximum-swap.java
@@ -0,0 +1,32 @@
+class Solution {
+    //this one is good
+//     Most of other solutions use a bucket to store the last occurrence of a digit and do a comparison for each digit. However, that is not efficient for case like "1000000"
+
+// The idea is straightforward, as we can simply scan the digits backward and record the position of the largest digit when it first appears.
+
+// Next time we scan the digits from left to right and find the first digit that is less than max to do the swap.
+    public int maximumSwap(int num) {
+         char[] digits = String.valueOf(num).toCharArray();
+        int[] maxIdx = new int[digits.length];
+        int maxPos = digits.length - 1;
+        maxIdx[maxPos] = maxPos;
+        
+        for (int i = digits.length - 2; i >= 0; i--) {
+            if (digits[i] > digits[maxPos]) {
+                maxPos = i;
+            }
+            maxIdx[i] = maxPos;
+        }
+        
+        for (int i = 0; i < digits.length; i++) {
+            if (digits[i] != digits[maxIdx[i]]) {
+                char tmp = digits[i];
+                digits[i] = digits[maxIdx[i]];
+                digits[maxIdx[i]] = tmp;
+                return Integer.parseInt(String.valueOf(digits));
+            }
+        }
+        
+        return num;
+    }
+}
