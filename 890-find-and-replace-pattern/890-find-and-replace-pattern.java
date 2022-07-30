ad0332019dea8cda3e1b8c6c46e91c5d793c42a5
PrashanPal <78657258+PrashanPal@users.noreply.github.com>
2022-07-30 18:04:19 +0530

    Time: 3 ms (60.70%), Space: 43.4 MB (28.27%) - LeetHub

---
+++ 890-find-and-replace-pattern/890-find-and-replace-pattern.java
@@ -0,0 +1,23 @@
+class Solution {
+    //very important tool
+    //n2--123
+    //approach:create code for pattern given and now create code for all the words ,if the code for word is same as the code code for pattern the put this word int the list of answers
+     public List<String> findAndReplacePattern(String[] words, String pattern) {
+        int[] p = F(pattern);
+        List<String> res = new ArrayList<String>();
+        for (String w : words)
+            if (Arrays.equals(F(w), p)) res.add(w);
+        return res;
+    }
+
+    public int[] F(String w) {
+        HashMap<Character, Integer> m = new HashMap<>();
+        int n = w.length();
+        int[] res = new int[n];
+        for (int i = 0; i < n; i++) {
+            m.putIfAbsent(w.charAt(i), m.size());
+            res[i] = m.get(w.charAt(i));
+        }
+        return res;
+    }
+}
