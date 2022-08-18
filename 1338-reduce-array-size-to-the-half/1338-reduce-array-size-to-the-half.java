f8b4c73b130ddff6b3dd4148d7bbacc85b44646a
PrashanPal <78657258+PrashanPal@users.noreply.github.com>
2022-08-18 09:28:35 +0530

    Time: 75 ms (69.20%), Space: 82.7 MB (72.99%) - LeetHub

--- 1338-reduce-array-size-to-the-half/1338-reduce-array-size-to-the-half.java
+++ 1338-reduce-array-size-to-the-half/1338-reduce-array-size-to-the-half.java
@@ -1,34 +1,27 @@
+//Hi, here I am sharing O(nlogn) Java solution. If any suggestion or question, feel free to ask. :)
+//this solution is based on priorityque ,since we only need to return the count of integers in the set ,not the list of integers 
 class Solution {
     public int minSetSize(int[] arr) {
-        //approach: use a hashmap and count frequency of each integer ,no sort this hashmap in decreasing order on the basis of values i.e. in decreasing order of frequencies
-        HashMap<Integer,Integer> hm=new HashMap<>();
-        for(int i=0;i<arr.length;i++){
-            if(!hm.containsKey(arr[i]))
-                hm.put(arr[i],1);
-            else
-                hm.put(arr[i],hm.get(arr[i])+1);
-        }//for
-        //till here our map is completed
-        //each row of this array contains key and value
-        int a[][]=new int[hm.size()][2];
-        int i=0;
-        for(int e:hm.keySet()){
-            a[i][0]=e;
-            a[i][1]=hm.get(e);
-            i++;
+        
+        int n = arr.length;
+        if(n==1 || n==2) return 1;
+        
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());//it will contains frequecies on elements of array in the decreasing order
+        
+        HashMap<Integer, Integer> map = new HashMap<>();
+        
+        for(Integer x: arr){
+            map.put(x,map.getOrDefault(x, 0) + 1);
         }
-        //till here ourarray is completed
-        //sorting the array on the basis of values
-        Arrays.sort(a,(p,q)->Integer.compare(q[1],p[1]));
-        int count=0;
-        int t=0;
-        int j=0;
-        if(arr.length%2==0) t=arr.length/2;
-        else t=arr.length/2+1;
-        while(count<t){
-            count+=a[j][1];
-            j++;
-        }//while
-        return j;
+        for(Integer x: map.values()){
+            pq.offer(x);
+        }
+        int res = 0;
+        int cnt = 0;
+        while(res < n/2 && pq.size() > 0) {
+            res += pq.poll();
+            cnt++;
+        }
+        return cnt;
     }
 }
