4052a7fefbe1efde2bf4dec8b95c5eb8094d6245
PrashanPal <78657258+PrashanPal@users.noreply.github.com>
2022-08-18 10:54:45 +0530

    Time: 4 ms (83.63%), Space: 44.7 MB (80.18%) - LeetHub

--- 2273-find-resultant-array-after-removing-anagrams/2273-find-resultant-array-after-removing-anagrams.java
+++ 2273-find-resultant-array-after-removing-anagrams/2273-find-resultant-array-after-removing-anagrams.java
@@ -1,47 +1,20 @@
 class Solution {
+    //this approach is much better then mine
     public List<String> removeAnagrams(String[] words) {
-        List<String> l=new ArrayList<>();
-        for(String e:words){
-            l.add(e);
-        }//for
-        int i=1;
-        while(i<l.size()){
-            if(check(l.get(i),l.get(i-1))){
-                l.remove(i);
-                i=1;
-            }//if
-            else
-            i++;
-        }//while
-        return l;
-    }//fn
-          public boolean check(String s, String t) {
-        //using hashmap
-        HashMap<Character,Integer> h=new HashMap<>();
-        for(int i=0;i<s.length();i++){
-            char c=s.charAt(i);
-            if(h.containsKey(c))
-                h.put(c,h.get(c)+1);
-            else
-                h.put(c,1);
-        }
-        boolean b=true;
-       for(int i=0;i<t.length();i++){
-            char c=t.charAt(i);
-            if(h.containsKey(c))
-                h.put(c,h.get(c)-1);
-           else
-               b=false;
-        }//for
-            
-            for(Character i:h.keySet()){
-                if(h.get(i)!=0){
-                     b=false;
-                }//if
-            }//for
-        // if(h.size()=0){
-        //     b=false;
-        // }
-            return b;
-    }//check
+        String temp="";
+            List<String>res=new ArrayList<>();
+            for(String s:words){
+                    char[]arr=s.toCharArray();
+                    Arrays.sort(arr);
+                    String sorted=new String(arr);
+                    if(temp.equals(sorted))continue;
+                    else{
+                            res.add(s);
+                    }
+                    temp=sorted;
+                   
+            }
+            return res;
+    }
+    
 }
