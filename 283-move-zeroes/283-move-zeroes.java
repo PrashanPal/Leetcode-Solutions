a54d32a5b7e2e42910b0a3afe47637f9b8a6f499
PrashanPal <78657258+PrashanPal@users.noreply.github.com>
2022-08-30 23:58:03 +0530

    Time: 26 ms (12.25%), Space: 43.5 MB (98.05%) - LeetHub

--- 283-move-zeroes/283-move-zeroes.java
+++ 283-move-zeroes/283-move-zeroes.java
@@ -1,15 +1,36 @@
 class Solution {
+    //approach: simple two pointers apprach is used 
+    // take two pointer s=0 and j=0
+    //nums[s]!=0 --> i.e no need  to swap
+    //if nums[s]==0 ,then j=s+1 , and increment j while(j<n&&nums[j]==0) , then if
+    //j>n ,ie, no non zero elment to swap simple break
+    //if(j<n && nums[j]!=0) swap the elements at s and jth index
     public void moveZeroes(int[] nums) {
-     int j=0;
-        for(int i=0;i<nums.length;i++){
+        int s=0;
+        int j=0;
+        int n=nums.length;
+        for(int i=0;i<n;i++){
             if(nums[i]!=0){
-                nums[j]=nums[i];
-                j++;
+                continue;
             }
-        }
-        while(j<nums.length){
-            nums[j]=0;
-            j++;
-        }
+            else if(nums[i]==0){
+                s=i;
+                j=s+1;
+                while(j<n&&nums[j]==0){
+                    j++;
+                }//while
+                if(j<n&&nums[j]!=0){
+                    swap(nums,i,j);
+                }//if
+                if(j>=n) break;
+                //else if(j<n) i=j-1;
+            }//else if
+        }//for
+        
+    }//fn
+    public void swap(int nums[],int i,int j){
+        int t=nums[i];
+        nums[i]=nums[j];
+        nums[j]=t;
     }
 }
