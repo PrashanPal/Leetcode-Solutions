e8035ce7718fa0f3808f17b682d466080e4e59d0
PrashanPal <78657258+PrashanPal@users.noreply.github.com>
2022-08-30 19:49:46 +0530

    Time: 12 ms (53.92%), Space: 46.1 MB (34.93%) - LeetHub

--- 151-reverse-words-in-a-string/151-reverse-words-in-a-string.java
+++ 151-reverse-words-in-a-string/151-reverse-words-in-a-string.java
@@ -1,10 +1,29 @@
 class Solution {
+    // this is method 2: this method is not much efficient as method1 , and complex also 
+    //approach: here i dont use string.trim function  , here i have taken two substrings sb for storing final result and string st ,for forming words in a string a and each time on completion of st , i insert st on the 0th position of sb followed by blank space , to seperate two words
     public String reverseWords(String s) {
-        String[] parts = s.trim().split("\\s+");// i dont understand clearly about this line please                                                        inspect it
-String out = "";
-for (int i = parts.length - 1; i > 0; i--) {
-    out += parts[i] + " ";
-}
-return out + parts[0];
+        StringBuilder sb=new StringBuilder("");
+        for(int i=0;i<s.length();i++){
+            StringBuilder st=new StringBuilder(""); 
+            char c=s.charAt(i);
+            if(c!=' '){
+                while(i<s.length()&&s.charAt(i)!=' '){
+                st.append(String.valueOf(s.charAt(i)));
+                    i++;
+                }//while
+                sb.insert(0,st);
+                i--;
+            }
+            else if(c==' '){
+                while(i<s.length()&&s.charAt(i)==' '){
+                    i++;
+                }//while
+                sb.insert(0,String.valueOf(' '));
+                i--; 
+            }//else if
+        }//for
+        if(sb.charAt(0)==' ') sb.deleteCharAt(0);
+        if(sb.charAt(sb.length()-1)==' ') sb.deleteCharAt(sb.length()-1);
+        return sb.toString();
     }
 }
