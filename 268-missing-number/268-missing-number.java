02533b078c3f50741c86b361ce06bd3a144d5975
PrashanPal <78657258+PrashanPal@users.noreply.github.com>
2022-09-02 05:02:38 +0530

    Time: 1 ms (78.38%), Space: 51.9 MB (12.01%) - LeetHub

--- 268-missing-number/268-missing-number.java
+++ 268-missing-number/268-missing-number.java
@@ -1,9 +1,15 @@
 class Solution {
+    //since in this question it is given that all the no. are distict and all the no. in the array is in range [0,n] and only one no. is missing ---> so here n(n-1)/2 approach will work
+    // but also remember that this approach will not work if the no. is both missing and repeating
     public int missingNumber(int[] nums) {
-        int  s=0;
-        for(int i=1;i<=nums.length;i++)
-            s+=i;
-        for(int i:nums) s-=i;
-        return s;
+        //using xor approach ,since no. is only missing and not repeating
+        int r=0;
+        for(int i=0;i<nums.length;i++){
+            r=r^nums[i];
+        }
+        for(int i=0;i<=nums.length;i++){
+            r=r^i;
+        }
+        return r;
     }
 }
