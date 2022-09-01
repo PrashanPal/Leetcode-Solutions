af86a4aba9e62f882a5e2e956c004eec266c48bf
PrashanPal <78657258+PrashanPal@users.noreply.github.com>
2022-09-02 00:59:56 +0530

    Time: 1 ms (87.01%), Space: 64.8 MB (17.32%) - LeetHub

--- 189-rotate-array/189-rotate-array.java
+++ 189-rotate-array/189-rotate-array.java
@@ -1,35 +1,20 @@
 class Solution {//450
     //aptimzed approach: 1st reverse the entire array ,then reverse elements form 0 to k-1 and then form k to n-1 ,here time complexity is o(n);
+   
     public void rotate(int[] nums, int k) {
-         k=k%nums.length;
-        //step1: reverse the whole array from 0 to n-1
-        int i=0,j=nums.length-1;
-        while(i<j){
-            int t=nums[i];
-            nums[i]=nums[j];
-            nums[j]=t;
-            i++;
-            j--;
-        }//while
-        // //step2: reverse the whole array from 0 to k-1
-         i=0;
-         j=k-1;
-         while(i<j){
-            int t=nums[i];
-            nums[i]=nums[j];
-            nums[j]=t;
-            i++;
-            j--;
-        }//while
-         //step3: reverse the whole array from k to n-1
-         i=k;
-         j=nums.length-1;
-         while(i<j){
-            int t=nums[i];
-            nums[i]=nums[j];
-            nums[j]=t;
-            i++;
-            j--;
-        }//while
-    }
+    k %= nums.length;
+    reverse(nums, 0, nums.length - 1);
+    reverse(nums, 0, k - 1);
+    reverse(nums, k, nums.length - 1);
 }
+
+public void reverse(int[] nums, int start, int end) {
+    while (start < end) {
+        int temp = nums[start];
+        nums[start] = nums[end];
+        nums[end] = temp;
+        start++;
+        end--;
+    }//while
+}//reverse
+}//class
