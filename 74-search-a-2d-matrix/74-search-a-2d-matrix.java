954acb497652eb6bc681af6acd498d00334aa162
PrashanPal <78657258+PrashanPal@users.noreply.github.com>
2022-09-02 06:13:55 +0530

    Time: 0 ms (100.00%), Space: 41.9 MB (94.73%) - LeetHub

--- 74-search-a-2d-matrix/74-search-a-2d-matrix.java
+++ 74-search-a-2d-matrix/74-search-a-2d-matrix.java
@@ -1,16 +1,29 @@
 class Solution {
-      public boolean searchMatrix(int[][] matrix, int target) {
-            int i = 0, j = matrix[0].length - 1;
-            while (i < matrix.length && j >= 0) {
-                    if (matrix[i][j] == target) {
-                        return true;
-                    } else if (matrix[i][j] > target) {
-                        j--;
-                    } else {
-                        i++;
+//     Integers in each row are sorted from left to right.
+// The first integer of each row is greater than the last integer of the previous row.
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int n=matrix.length;
+        int m=matrix[0].length;
+        
+        //find the appropriate row to search the target
+        for(int i=0;i<n;i++){
+            if(matrix[i][0]<=target&&matrix[i][m-1]>=target){
+                //now apply binary search in this array to search the target
+                int s=0;
+                int e=m-1;
+                int mid=0;
+                while(s<=e){
+                    mid=(s+e)/2;
+                if(target==matrix[i][mid]) return true;
+                    else if(target<matrix[i][mid]){
+                        e=mid-1;
                     }
-                }
-            
-            return false;
-        }
+                     else if(target>matrix[i][mid]){
+                        s=mid+1;
+                    }//else if
+                }//while
+            }//if
+        }//for
+        return false;
+    }
 }
