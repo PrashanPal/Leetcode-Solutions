38ffe2d16c70feb5b09d05310bc281cdd09f96c4
PrashanPal <78657258+PrashanPal@users.noreply.github.com>
2022-09-09 01:00:45 +0530

    Time: 3 ms (7.23%), Space: 42.6 MB (76.61%) - LeetHub

--- 88-merge-sorted-array/88-merge-sorted-array.java
+++ 88-merge-sorted-array/88-merge-sorted-array.java
@@ -1,31 +1,30 @@
 class Solution {
-    public void merge(int[] n1, int m, int[] n2, int n) {
-        int a[]=new int[n+m];
-        int i=0,j=0,k=0;
-        while(i<m||j<n){
-            if(i<m&&j<n&&n1[i]<=n2[j]){
-                a[k]=n1[i];
-                k++;
-                i++;
+    //s450---> arrays hard
+    //without using extra space
+    //time: o(m*n) space:o(1)
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        //according to question nums1.length=n+m
+        if(n==0) return;
+        for(int i=0;i<m;i++){
+            if(nums1[i]<=nums2[0]){
+                continue;
             }
-            else if(i<m&&j<n&&n1[i]>n2[j]){
-                a[k]=n2[j];
-                k++;
-                j++;
-            }
-            else if(i==m&&j<n){
-                a[k]=n2[j];
-                k++;
-                j++;
-            }
-            else if(i<m&&j==n){
-                a[k]=n1[i];
-                k++;
-                i++;
-            }
-        }//while
-        for(int p=0;p<n+m;p++){
-            n1[p]=a[p];
-        }
+            else if(nums1[i]>nums2[0]){
+                int t=nums1[i];
+                nums1[i]=nums2[0];
+                nums2[0]=t;
+                // now rearrange nums2 in sorted order using insertion technique
+                int p=0;
+                while(p+1<n&&nums2[p]>nums2[p+1]){// time: o(n);
+                    int r=nums2[p];
+                    nums2[p]=nums2[p+1];
+                    nums2[p+1]=r;
+                    p++;
+                }//while
+            }//else if
+        }//for
+        for(int i=0;i<n;i++){
+            nums1[m+i]=nums2[i];
+        }//for
     }
 }
